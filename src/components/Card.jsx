dbe14a110a230998e77b8f31fa1b4111bb4325f4
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-07-30 15:41:27 -0300

    add styles to the card's, rename icons and add icons check and delete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,7 @@
+import check from '../assets/check.png'
+import eliminate from '../assets/eliminate.png'
+
+
 export default function Card({title, id, deleteTask, changeTask}){
     //funtions
     const handleDelete = () => {
@@ -7,10 +11,16 @@ export default function Card({title, id, deleteTask, changeTask}){
         changeTask(id)
     }
     return(
-        <div>
-            <p>{title}</p>
-            <button onClick={handleChange}>Check</button>
-            <button onClick={handleDelete}>Delete</button>
-        </div>
+        <section className="flex justify-center gap-4 mb-4">
+            <p className='p-2 bg-[#fff] rounded'>{title}</p>
+            <div className='flex gap-2'>
+                <button onClick={handleChange}>
+                    <img src={check} alt="Check" className='w-7' />
+                </button>
+                <button onClick={handleDelete}>
+                    <img src={eliminate} alt="Delete" className='w-7' />
+                </button>
+            </div>
+        </section>
     )
 }
