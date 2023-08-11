e5aec864b6a44e6bf6f8c5951919f4497da9e2f3
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-08-11 13:38:44 -0300

    modified margin in the ContainCard component

--- src/components/ContainCard.jsx
+++ src/components/ContainCard.jsx
@@ -6,11 +6,11 @@ export default function ContainCard({tasks, option, deleteTask, changeTask}){
     const filteredTask = option === "" ? tasks : tasks.filter(task => task.condition === option)
     
     return(
-        <section className="flex flex-col relative">
+        <section className="mt-20 flex flex-col relative">
             <figure className="flex flex-col absolute self-center top-[-70px] z-[2]">
                 <img src={Clip} alt="Hook Papel" />
             </figure>
-            <div className="w-[90%] flex flex-col self-center relative bg-[#fff] h-[40px] mb-20 lg:w-1/2"></div>
+            <div className="w-[90%] flex flex-col self-center relative bg-[#fff] h-[40px] lg:w-1/2"></div>
                 {filteredTask &&
                 filteredTask.map((task, index) => {
                     return (
